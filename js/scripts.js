19081f13dbe2495f23a12c9bfa4a605ff3406fa0
Brynna <brynna.mccarter@gmail.com>
2017-01-31 15:55:29 -0800

    add accrue prototype to calculate final results

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
 var tracker = 1;
+var score;
+
+function Player(name, score) {
+  this.name = name;
+  this.score = score;
+};
+
+
+  Player.prototype.accrue = function() {
+    if (tracker >= 5) {
+      if (this.score >= 2) {
+        $("p#end-positive-show").show();
+      } else if (this.score < 2 && this.score >= 0) {
+        $("p#end-neutral-show").show();
+      } else if (this.score < 0) {
+        $("p#end-negative-show").show();
+      }
+  };
+};
+
 
 //user interface logic
 $(document).ready(function() {
-
   //this fires when ANY answer is clicked
+  score = 0;
 
   $("div[class^='answers']").click(function(event) {
     event.preventDefault();
@@ -11,14 +31,24 @@ $(document).ready(function() {
     var answerShow = this.id + "-show";
     var questionSelector = window[approvalRating];
     console.log(approvalRating);
-    console.log(answerShow);
-    console.log(this.id);
+    if (approvalRating === "frame" + tracker + "-positive") {
+      score += 1;
+
+      console.log(score);
+    } else if (approvalRating === "frame" + tracker + "-negative") {
+      score -= 1;
+
+    }
+    console.log(score);
+    // console.log(answerShow);
+    // console.log(this.id);
     $("#" + answerShow).show();
     $(".frame"+tracker+"-answers").hide();
-    console.log(tracker);
+    // console.log(tracker);
     tracker += 1;
-    console.log(tracker);
+    // console.log(tracker);
     $(".frame"+tracker+"-answers").show();
-
   });
+  var newPlayer = new Player(name, score);
+  var playerScore = newPlayer.accrue();
 });
